ba46a6c97420f6e53ef90aaafd1d9ebbff50e554
panagiotisky <panagiotisky@users.noreply.github.com>
2015-04-28 19:59:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ var accordionsNum = document.querySelectorAll('section.accordion'), i;
 for (i = 0; i < accordionsNum.length; i++) {
   var articlesNum = accordionsNum[i].querySelectorAll('article.accordion-article'), j;
   for (j = 0; j < articlesNum.length; j++) {
-    articlesNum[j].addEventListener('click', function(e) {
-      if (e.target.parentNode.parentNode.querySelector('.selected')) {
-        e.target.parentNode.parentNode.querySelector('.selected').className = 'accordion-article';
+    articlesNum[j].getElementsByTagName('h2')[0].addEventListener('click', function(e) {
+      if (e.target.parentNode.className.indexOf('selected') > -1) {
+        e.target.parentNode.classList.toggle('selected');
+      } else {
+        if (e.target.parentNode.parentNode.querySelector('.selected')) {
+          e.target.parentNode.parentNode.querySelector('.selected').classList.toggle('selected');
+        }
+        e.target.parentNode.classList.toggle('selected');
       }
-      e.target.parentNode.classList.toggle('selected');
     });
   }
 }
